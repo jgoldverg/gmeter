31acca3b7014e8b18fa0318d68cde61569199509
jgoldverg <jgoldverg@gmail.com>
2025-04-11 16:30:49 +0000

    Aright things are finally working with simple counter!

--- cmd/gmeter/main.go
+++ cmd/gmeter/main.go
@@ -3,71 +3,45 @@ package main
 import (
 	"flag"
 	"log"
-	"net"
 	"os"
 	"os/signal"
-	"syscall"
 	"time"
 
-	"github.com/cilium/ebpf/link"
-	"github.com/cilium/ebpf/rlimit"
 	"gmeter/gen"
 )
 
 func main() {
-	// Parse CLI args
-	ifaceName := flag.String("i", "eth0", "Network interface to monitor")
+	iface := flag.String("i", "eno1np0:", "Network interface")
 	flag.Parse()
 
-	// Remove memory limits
-	if err := rlimit.RemoveMemlock(); err != nil {
-		log.Fatalf("Failed to remove memlock limit: %v", err)
-	}
-
-	// Load eBPF objects
-
-	var objs gen.CounterObject
-	if err := gen.loadCounter(&objs, nil); err != nil {
-		log.Fatalf("Loading BPF objects: %v", err)
-	}
-	defer objs.Close()
-
-	// Get network interface
-	iface, err := net.InterfaceByName(*ifaceName)
+	counter, err := gen.NewCounter()
 	if err != nil {
-		log.Fatalf("Failed to get interface %s: %v", *ifaceName, err)
+		log.Fatalf("Failed to create counter: %v", err)
+	}
+	defer counter.Close()
+
+	if err := counter.Attach(*iface); err != nil {
+		log.Fatalf("Failed to attach: %v", err)
 	}
 
-	// Attach XDP program
-	l, err := link.AttachXDP(link.XDPOptions{
-		Program:   objs.CountPackets,
-		Interface: iface.Index,
-		Flags:     link.XDPGenericMode,
-	})
-	if err != nil {
-		log.Fatalf("Failed to attach XDP program: %v", err)
-	}
-	defer l.Close()
+	log.Printf("Counting packets on %s...", *iface)
 
-	log.Printf("Counting packets on %s (Ctrl+C to exit)", *ifaceName)
-
-	// Setup monitoring
 	tick := time.NewTicker(1 * time.Second)
 	defer tick.Stop()
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
 
 	for {
 		select {
 		case <-tick.C:
-			var count uint64
-			if err := objs.PktCount.Lookup(uint32(0), &count); err != nil {
-				log.Printf("Error reading counter: %v", err)
+			count, err := counter.ReadCount()
+			if err != nil {
+				log.Printf("Error reading count: %v", err)
 				continue
 			}
-			log.Printf("Packets received: %d", count)
-		case <-stop:
-			log.Println("Detaching and exiting...")
+			log.Printf("Packets: %d", count)
+		case <-sig:
 			return
 		}
 	}
